36c585227e33aa491b83d7a502e469c6c74cec92
Volha Kulevich <volha.kulevich@gmail.com>
2019-02-07 01:11:25 +0300

    added axios

--- src/services/api/adverts.js
+++ src/services/api/adverts.js
@@ -1,15 +1,14 @@
+import Axios from 'axios';
+
 export function getAdverts() {
   return new Promise((resolve, reject) => {
-    setTimeout(
-      () => resolve([
-        { id: 1, category: 'Sporting Goods', price: 49.99, title: 'Football' },
-        { id: 2, category: 'Sporting Goods', price: 9.99, title: 'Baseball' },
-        { id: 3, category: 'Sporting Goods', price: 29.99, title: 'Basketball' },
-        { id: 4, category: 'Electronics', price: 99.99, title: 'iPod Touch' },
-        { id: 5, category: 'Electronics', price: 399.99, title: 'iPhone 5' },
-        { id: 6, category: 'Electronics', price: 199.99, title: 'Nexus 7' }
-      ]),
-      1000
-    );
+    Axios.get('http://localhost:3000/api/adverts').then(response => {
+      resolve(response.data.adverts.map(advert => { return {
+        id: advert._id,
+        category: advert.category,
+        price: advert.price,
+        title: advert.title,
+      }}));
+    });
   });
 }
