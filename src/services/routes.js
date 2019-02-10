229f08055eadb34e648f196f8de49106593f0906
Volha Kulevich <volha.kulevich@gmail.com>
2019-02-10 23:04:35 +0300

    removed parent div from App

--- src/services/routes.js
+++ src/services/routes.js
@@ -5,19 +5,17 @@ import {App, Main, Adverts, Authors, Advert, Author, AdvertEdit, AuthorEdit, Adv
 } from '../containers';
 
 export const Routes = () => (
-  <div>
-    <App>
-      <Switch>
-        <Route exact path={PATHS.advertNew.path} component={AdvertAdd}/>
-        <Route exact path={PATHS.authorNew.path} component={AuthorAdd}/>
-        <Route exact path={PATHS.main.path} component={Main}/>
-        <Route exact path={PATHS.adverts.path} component={Adverts}/>
-        <Route exact path={PATHS.authors.path} component={Authors}/>
-        <Route exact path={PATHS.advert.path} component={Advert}/>
-        <Route exact path={PATHS.author.path} component={Author}/>
-        <Route exact path={PATHS.advertEdit.path} component={AdvertEdit}/>
-        <Route exact path={PATHS.authorEdit.path} component={AuthorEdit}/>
-      </Switch>
-    </App>
-  </div>
+  <App>
+    <Switch>
+      <Route exact path={PATHS.advertNew.path} component={AdvertAdd}/>
+      <Route exact path={PATHS.authorNew.path} component={AuthorAdd}/>
+      <Route exact path={PATHS.main.path} component={Main}/>
+      <Route exact path={PATHS.adverts.path} component={Adverts}/>
+      <Route exact path={PATHS.authors.path} component={Authors}/>
+      <Route exact path={PATHS.advert.path} component={Advert}/>
+      <Route exact path={PATHS.author.path} component={Author}/>
+      <Route exact path={PATHS.advertEdit.path} component={AdvertEdit}/>
+      <Route exact path={PATHS.authorEdit.path} component={AuthorEdit}/>
+    </Switch>
+  </App>
 );
