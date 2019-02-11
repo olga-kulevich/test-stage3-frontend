9296461702cf216f8075001d9574b2dff54011a2
fermata <olga.bialotskaya@gmail.com>
2019-02-11 15:47:35 +0300

    added validation to form

--- src/components/AdvertForm/AdvertForm.js
+++ src/components/AdvertForm/AdvertForm.js
@@ -7,46 +7,103 @@ class AdvertForm extends PureComponent {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      fields: {
+        title: this.props.advert.title,
+        category: this.props.advert.category,
+        price: this.props.advert.price
+      },
+      errors: {}
+    }
+  }
+
+  handleValidation(){
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Title
+    if(!fields["title"]) {
+      formIsValid = false;
+      errors["title"] = "Enter new Title";
+    }
+
+    if(typeof fields["title"] !== "undefined"){
+      if(!fields["title"].match(/^[a-zA-Z0-9_.-]*$/)){
+        formIsValid = false;
+        errors["title"] = "Only letters, numbers, '-', '.'";
+      }
+    }
+
+    //Category
+    if(typeof fields["category"] !== "undefined"){
+      if(!fields["category"].match(/^[a-zA-Z0-9_.-]*$/)){
+        formIsValid = false;
+        errors["category"] = "Only letters, numbers, '-', '.'";
+      }
+    }
+
+    //Price
+    if(typeof fields["price"] !== "undefined"){
+      if(!fields["price"].match(/^[1-9]\d*$/)){
+        formIsValid = false;
+        errors["price"] = "Only positive numeric value";
+      }
+    }
+
+    this.setState({errors: errors});
+    return formIsValid;
   }
 
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
+    let fields = this.state.fields;
+    fields[name] = value;
+    this.setState({fields});
     this.props.handleStateChange({
       [name]: value
     });
+
   }
 
   handleSubmit(event) {
     event.preventDefault();
     const { onSubmit } = this.props;
-    onSubmit();
+    if(this.handleValidation()){
+      onSubmit();
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          Title
-          <input type="text" name="title" value={this.props.advert.title} onChange={this.handleInputChange}/>
-        </label>
+        <div className="center">
+          <label>Title</label>
+          <input type="text" name="title" size="60" className="inputs" value={this.props.advert.title} onChange={this.handleInputChange}/>
+        </div>
+        <div className="center" style={{color: "red"}}>{this.state.errors["title"]}</div>
 
-        <label>
-          Category
-          <input type="text" name="category" value={this.props.advert.category} onChange={this.handleInputChange}/>
-        </label>
+        <div className="center">
+          <label>Category</label>
+          <input type="text" name="category" size="60" className="inputs" value={this.props.advert.category} onChange={this.handleInputChange}/>
+        </div>
+        <div className="center" style={{color: "red"}}>{this.state.errors["category"]}</div>
 
-        <label>
-          Price
-          <input type="number" name="price" value={this.props.advert.price} onChange={this.handleInputChange}/>
-        </label>
+        <div className="center">
+          <label>Price</label>
+          <input type="number" name="price" size="60" className="inputs" value={this.props.advert.price} onChange={this.handleInputChange}/>
+        </div>
+        <div className="center" style={{color: "red"}}>{this.state.errors["price"]}</div>
 
-        <Button value='Save'/>
-        <Button value='Cancel' onClick={(event) => {
-          event.preventDefault();
-          this.props.onCancelClick();
-        }}/>
+        <div className="center">
+          <Button value='Save'/>
+          <Button value='Cancel' onClick={(event) => {
+            event.preventDefault();
+            this.props.onCancelClick();
+          }}/>
+        </div>
       </form>
     );
   }
